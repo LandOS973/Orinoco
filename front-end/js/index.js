732c4b6eb7c9d4c7d8267cd9ad57c5144b917fc0
Thomas Landais <thomas.landais9733@gmail.com>
2021-08-23 17:25:44 +0200

    fin page acceuil début page produit

--- front-end/js/index.js
+++ front-end/js/index.js
@@ -1,30 +1,26 @@
-function getArticles(){
-    return fetch("http://localhost:3000/api/teddies")
-        .then(response => response.json())
-        .then(articles => {return articles})
-        .catch(err => console.log(err))
-}
-
 function afficherArticles(articles){
     for (const article of articles) {
         document.querySelector('.product').innerHTML += `
-        <div class="col-4">
-            <div class="card" style="width: 18rem;">
-                <img class="card-img-top" src="${article.imageUrl}" alt="Card image cap">
-                <div class="card-body">
-                    <h5 class="card-title">${article.name}</h5>
-                    <p class="card-text">${article.description}</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
+        <div class="col-sm-12 col-md-6 col-lg-4">
+            <div class="card">
+                <a href="./article.html?id=${article._id}">
+                    <img class="card-img-top" src="${article.imageUrl}" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="price">${article.price/100} &euro;</h5>
+                        <h5 class="card-title">${article.name}</h5>
+                        <div class="description">
+                            <p class="card-text">${article.description}</p>
+                        </div>
+                    </div>
+                </a>
             </div>
-        </div>
-        `;
+        </div>`;
     }
 }
 
 
 async function main(){
-    const articles = await getArticles();
+    const articles = await getArticles("http://localhost:3000/api/teddies");
     afficherArticles(articles);
 }
 
