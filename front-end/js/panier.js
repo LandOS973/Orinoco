278b39ef59c0a897647c2ca681ce1d7daa7d5461
Thomas Landais <thomas.landais9733@gmail.com>
2021-08-24 15:25:54 +0200

    ajout visuel panier

--- front-end/js/panier.js
+++ front-end/js/panier.js
@@ -1,6 +1,6 @@
 function affichagePanier(panier){
     if (!panier) {
-        document.querySelector('.panier').innerHTML += "Panier vide !";
+        document.querySelector('.panier').innerHTML += `<p class="text-center">Panier vide !</p><img class="travolta" src="../back-end/images/travolta.gif" alt="" srcset="">`;
     }else{
         for (const article of panier) {
             document.querySelector('.panier').innerHTML += `
@@ -20,6 +20,31 @@ function affichagePanier(panier){
             </div>
             `
         }
+    }
+}
+
+//Calcul et Affiche prix total de la commande
+function total(panier){
+    if (panier) {
+        let total = 0;
+        for (const article of panier) {
+            total += article.price;
+        }
+        total/= 100;
+        document.querySelector('.total').innerHTML += `
+        <div class="col-sm-6 col-lg-4 mb-4">
+            <div class="card">
+                <div class="card-body">
+                    <strong>Prix total</strong> : ${total} &euro;
+                </div>
+            </div>
+        </div>`
+    }
+}
+
+//Affiche tout les component en rapport avec le formulaire
+function affichageForm(panier){
+    if(panier){
         document.querySelector('.validation').innerHTML +=`<btn href="#" class="retrait margin col-2 mb-4 btn btn-danger">Effacer le panier</btn>`;
         document.querySelector('.validation').innerHTML +=`<btn href="#" class="valider margin col-2 mb-4 btn btn-success">Valider la commande</btn>`;
     }
@@ -38,6 +63,8 @@ function supprimer(){
 function main(){
     let panier = JSON.parse(localStorage.getItem("panier"));
     affichagePanier(panier);
+    affichageForm(panier);
+    total(panier);
     supprimer(); 
 }
 
